0f1fa19d8926d3581ad0e655072d72cbb168fd01
Alex Cooper <616126+bondarev@users.noreply.github.com>
2023-06-09 12:40:03 +0400

    Add 3.5 task solution

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,3 +1,11 @@
 function getMinMax(str) {
-  // ваш код...
+  const numbers = str
+    .split(' ')
+    .map(value => parseFloat(value))
+    .filter(value => !isNaN(value));
+
+  return {
+    min: Math.min(...numbers),
+    max: Math.max(...numbers),
+  };
 }
